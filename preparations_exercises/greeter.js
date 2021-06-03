89586954eccc4c3fdeb110c3a9bfe04a61976f93
Mike Wilson <mwilso17@my.westga.edu>
2021-06-03 07:07:40 -0400

    added additional console.log that pull variable

--- preparations_exercises/greeter.js
+++ preparations_exercises/greeter.js
@@ -1,3 +1,5 @@
 //this program greets someone three times using the string associated with the variable 'name'
 let name = 'Victor';
 console.log('Good Morning, ' + name + '.');
+console.log('Good Afternoon, ' + name + '.');
+console.log('Good Evening, ' + name + '.');
