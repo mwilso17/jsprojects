60c3f253630a7bcebdbf2874aef6a3a01e3cad38
Mike Wilson <mwilso17@my.westga.edu>
2021-06-03 08:18:45 -0400

    added code to show how to manipulate a variable

--- preparations_exercises/greetings.js
+++ preparations_exercises/greetings.js
@@ -1,2 +1,14 @@
 let greetingMessage = "Good Morning!";
-console.log(greetingMessage);
+
+function greetPeople() {
+  console.log(greetingMessage);
+}
+
+greetPeople();
+
+function changeGreetingMessage(newMessage) {
+  greetingMessage = newMessage;
+}
+
+changeGreetingMessage("Good Evening!");
+greetPeople();
