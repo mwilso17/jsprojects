082271682af8096c486a2b748894876ed3562062
Mike Wilson <mwilso17@my.westga.edu>
2021-06-03 07:45:27 -0400

    changed personalized_greetings for use in HTML

--- preparations_exercises/personalized_greetings.js
+++ preparations_exercises/personalized_greetings.js
@@ -1,5 +1,4 @@
 //lets users input their names and returns a personalized greeting
 
-let rlSync = require('readline-sync');
-let name = rlSync.question("What's your name?\n");
+let name = prompt("What's your name?\n");
 console.log(`Good Morning, ${name}!`);
