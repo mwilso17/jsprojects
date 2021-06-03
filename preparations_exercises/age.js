4a1acc78bbc29310a1e668df14c56d21db52598c
Mike Wilson <mwilso17@my.westga.edu>
2021-06-03 07:13:08 -0400

    added steps for 20, 30, and 40 years old

--- preparations_exercises/age.js
+++ preparations_exercises/age.js
@@ -3,3 +3,6 @@
 let age = 30; //age may be changed to any number. D
 
 console.log('In 10 years, you will be ' + (age + 10) + ' years old.');
+console.log('In 20 years, you will be ' + (age + 20) + ' years old.');
+console.log('In 30 years time, you will be ' + (age + 30) + ' years old.');
+console.log('In 40 years time, you will be ' + (age + 40) + ' years old.');
