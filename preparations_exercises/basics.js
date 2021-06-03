e89f193a3f04ae532baafda0ef1c8bf03159a48d
Mike Wilson <mwilso17@my.westga.edu>
2021-06-03 06:05:34 -0400

    added an array

--- preparations_exercises/basics.js
+++ preparations_exercises/basics.js
@@ -3,3 +3,6 @@ console.log('5' + 10) //this returns '510' since it there is a + and a string
 console.log(5 + 10) //this returns 15 since both inputs are numbers
 
 console.log(Number('5') + 10) //shows another way you can log this so that '5' is calculated as a number and not a string
+
+let petNames = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin'];
+//The code above creates an array that lists pet names
