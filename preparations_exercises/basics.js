ce493918e7697c6d799006b7b61c1723dfddd301
Mike Wilson <mwilso17@my.westga.edu>
2021-06-03 06:02:29 -0400

    added additional way to refactor code

--- preparations_exercises/basics.js
+++ preparations_exercises/basics.js
@@ -1,3 +1,5 @@
 console.log('5' + 10) //this returns '510' since it there is a + and a string
 
 console.log(5 + 10) //this returns 15 since both inputs are numbers
+
+console.log(Number('5') + 10) //shows another way you can log this so that '5' is calculated as a number and not a string
