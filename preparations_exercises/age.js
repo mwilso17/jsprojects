824195648ec6278f5f374fe795af354e0702074b
Mike Wilson <mwilso17@my.westga.edu>
2021-06-03 07:48:42 -0400

    updated age.js to allow for input

--- preparations_exercises/age.js
+++ preparations_exercises/age.js
@@ -1,6 +1,6 @@
 //this program calculates someones age in 10 year steps
 
-let age = 30; //age may be changed to any number. D
+let age = prompt('How old are you in years?'); //age may be changed to any number. D
 
 console.log('In 10 years, you will be ' + (age + 10) + ' years old.');
 console.log('In 20 years, you will be ' + (age + 20) + ' years old.');
